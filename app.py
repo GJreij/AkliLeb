498d1a837a595a2378277345b3d9bb04f9ac77b8
GJreij <georgesjreij46@gmail.com>
2025-09-24 19:04:27 +0200

    commit3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask on VS Code!!!!!"
+    return "Hello from Flask on VS Code!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 
 if __name__ == "__main__":
     app.run(debug=True)
