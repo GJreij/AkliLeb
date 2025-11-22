5c69c2f22907af8658e444d64ac018aa337d5e01
GJreij <georgesjreij46@gmail.com>
2025-11-22 21:18:42 +0100

    trial3

--- app.py
+++ app.py
@@ -1,38 +1,3 @@
-# from flask import Flask
-# from routes.macros_routes import macros_bp
-# from routes.mealplan_routes import mealplan_bp
-# from routes.checkout_summary import checkout_bp
-# from routes.confirm_order import confirm_order_bp
-# from routes.partners_routes import partner_bp
-# from routes.cooking import cooking_bp
-# from routes.ingredients import ingredients_bp
-# from routes.portioning import portioning_bp
-# from routes.packaging import packaging_bp
-# import os
-# app = Flask(__name__)
-
-# # Register blueprints
-# app.register_blueprint(macros_bp)
-# app.register_blueprint(confirm_order_bp)
-# app.register_blueprint(mealplan_bp)
-# app.register_blueprint(checkout_bp)
-# app.register_blueprint(partner_bp)
-# app.register_blueprint(ingredients_bp)
-# app.register_blueprint(cooking_bp)
-# app.register_blueprint(portioning_bp)
-# app.register_blueprint(packaging_bp)
-
-
-# @app.route("/")
-# def home():
-#     return "Hello from Flask API on Heroku!!"
-
-
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port)
-
-
 from flask import Flask
 from flask_cors import CORS
 from routes.macros_routes import macros_bp
