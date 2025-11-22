d4baa3b7a506b47096a36830680f3cc47de320b3
GJreij <georgesjreij46@gmail.com>
2025-11-22 21:04:47 +0100

    CORS added to heroku

--- utils/supabase_client.py
+++ utils/supabase_client.py
@@ -2,11 +2,11 @@ import os
 from supabase import create_client
 
 # --- Supabase setup ---
-# SUPABASE_URL = os.getenv("SUPABASE_URL")
-# SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-SUPABASE_URL = 'https://rjaypqibeymfopncjxkz.supabase.co'
-SUPABASE_KEY = '***REMOVED-LEAKED-SUPABASE-SERVICE-ROLE-KEY***'
+# SUPABASE_URL = 'https://rjaypqibeymfopncjxkz.supabase.co'
+# SUPABASE_KEY = '***REMOVED-LEAKED-SUPABASE-SERVICE-ROLE-KEY***'
 
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
