ac12dcf9f052ca13e1f910d252d5628394d0468f
GJreij <georgesjreij46@gmail.com>
2025-11-22 21:16:47 +0100

    trial2

--- utils/supabase_client.py
+++ utils/supabase_client.py
@@ -1,7 +1,6 @@
 import os
-from supabase import create_client
+from supabase import create_client, Client
 
-# --- Supabase setup ---
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
@@ -9,4 +8,6 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 # SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqYXlwcWliZXltZm9wbmNqeGt6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NDMwOTI0MiwiZXhwIjoyMDY5ODg1MjQyfQ.CCJT_Z7ZSYNzpcIyMhC0O7z8gVdaFEl-yj1cex2zxYY'
 
 
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
