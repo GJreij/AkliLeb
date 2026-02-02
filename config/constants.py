b755b8370f6491035e702224a884c8870db38a84
GJreij <georgesjreij46@gmail.com>
2026-02-02 16:27:02 +0200

    increased protein for balanced diet

--- config/constants.py
+++ config/constants.py
@@ -1,7 +1,7 @@
 # Define macro splits by diet type (percentages of kcal)
 DIET_MACROS = {
     "high_protein": {"protein": 0.3, "carbs": 0.40, "fat": 0.3},
-    "balanced": {"protein": 0.2, "carbs": 0.5, "fat": 0.3},
+    "balanced": {"protein": 0.25, "carbs": 0.45, "fat": 0.3},
     "low_fat": {"protein": 0.25, "carbs": 0.55, "fat": 0.20},
     "high_carbs": {"protein": 0.20, "carbs": 0.60, "fat": 0.20},
 }
